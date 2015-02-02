a1652c8135e1449440d71c40cb0b8cb06cd43279
Bogdan Leonte <bleonte@optaros.com>
2015-02-02 13:39:36 +0200

    Remove key and add from env_name

--- recipes/yum.rb
+++ recipes/yum.rb
@@ -1,5 +1,5 @@
 [tracelytics]
 name=Tracelytics
-baseurl=http://yum.tracelytics.com/793aa621-8484-496d-aff8-54ca97073cf3/6/x86_64
+baseurl=http://yum.tracelytics.com/node['traceview']['access_key']/6/x86_64
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-tracelytics
 gpgcheck=1
