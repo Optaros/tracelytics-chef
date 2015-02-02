b96a1bc0db06eb996ef1cd7c724e5633581d5c20
Bogdan Leonte <bleonte@optaros.com>
2015-02-02 17:15:13 +0200

    change repo url

--- recipes/yum.rb
+++ recipes/yum.rb
@@ -1,6 +1,6 @@
 require 'erb'
 [tracelytics]
 name=Tracelytics
-baseurl=http://yum.tracelytics.com/<%= node['traceview']['access_key'] %>/6/x86_64
+baseurl=http://yum.tracelytics.com/"<%= node['traceview']['access_key'] %>"/6/x86_64
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-tracelytics
 gpgcheck=1
