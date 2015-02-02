73906396e5f5b623c59c9f954c4343773ad63062
Bogdan Leonte <bleonte@optaros.com>
2015-02-02 17:51:45 +0200

    Change repo url

--- recipes/yum.rb
+++ recipes/yum.rb
@@ -1,6 +1,6 @@
 $key = node['traceview']['access_key']
 [tracelytics]
 name=Tracelytics
-baseurl=http://yum.tracelytics.com/793aa621-8484-496d-aff8-54ca97073cf3/6/x86_64
+baseurl=http://yum.tracelytics.com/'#{$key}'/6/x86_64
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-tracelytics
 gpgcheck=1
